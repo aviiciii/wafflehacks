2c3e2285962b284adfbdc9801c1434614400e095
Laaveshwaran P <laavesh1@gmail.com>
2023-06-25 14:32:39 +0530

    set up for hosting

--- extension/content.js
+++
@@ -1,7 +0,0 @@
-// content_script.js
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'getText') {
-    var text = document.body.innerText;
-    sendResponse({ text: text });
-  }
-});
